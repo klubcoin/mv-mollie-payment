07f857d9a6982698d8fc4edbcc29067058dff887
tonysviews <tonysviews@gmail.com>
2022-04-27 08:46:12 +0000

    Add the cet java source file : MoCustomer.javain the module : mv-mollie-payment

---
+++ facets/java/org/meveo/model/customEntities/MoCustomer.java
@@ -0,0 +1,114 @@
+package org.meveo.model.customEntities;
+
+import org.meveo.model.CustomEntity;
+import java.util.List;
+import org.meveo.model.persistence.DBStorageType;
+import java.time.Instant;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+public class MoCustomer implements CustomEntity {
+
+    public MoCustomer() {
+    }
+
+    public MoCustomer(String uuid) {
+        this.uuid = uuid;
+    }
+
+    private String uuid;
+
+    @JsonIgnore()
+    private DBStorageType storages;
+
+    private String mode;
+
+    private Instant createdAt;
+
+    private String metadata;
+
+    private String name;
+
+    private String id;
+
+    private String locale;
+
+    private String email;
+
+    @Override()
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public DBStorageType getStorages() {
+        return storages;
+    }
+
+    public void setStorages(DBStorageType storages) {
+        this.storages = storages;
+    }
+
+    public String getMode() {
+        return mode;
+    }
+
+    public void setMode(String mode) {
+        this.mode = mode;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getMetadata() {
+        return metadata;
+    }
+
+    public void setMetadata(String metadata) {
+        this.metadata = metadata;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getLocale() {
+        return locale;
+    }
+
+    public void setLocale(String locale) {
+        this.locale = locale;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override()
+    public String getCetCode() {
+        return "MoCustomer";
+    }
+}
