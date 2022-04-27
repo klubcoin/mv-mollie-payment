366af119a9f57fedd2aba6b6c5d4a2a78414fada
Tony Alejandro <tonysviews@gmail.com>
2022-04-27 17:15:03 +0800

    code cleanup

--- facets/java/io/liquichain/api/payment/WorkflowMetric.java
+++ facets/java/io/liquichain/api/payment/WorkflowMetric.java
@@ -9,50 +9,50 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.meveo.admin.exception.BusinessException;
+import org.meveo.api.persistence.CrossStorageApi;
 import org.meveo.model.customEntities.MoOrder;
 import org.meveo.model.storage.Repository;
 import org.meveo.service.storage.RepositoryService;
-import org.meveo.api.persistence.CrossStorageApi;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class WorkflowMetric extends Script {
-    
-    
-    private static final Logger log = LoggerFactory.getLogger(WorkflowMetric.class);
-    
-    private CrossStorageApi crossStorageApi = getCDIBean(CrossStorageApi.class);
-    private RepositoryService repositoryService = getCDIBean(RepositoryService.class);
-    private Repository defaultRepo = repositoryService.findDefaultRepository();
+    private static final Logger LOG = LoggerFactory.getLogger(WorkflowMetric.class);
+
+    private final CrossStorageApi crossStorageApi = getCDIBean(CrossStorageApi.class);
+    private final RepositoryService repositoryService = getCDIBean(RepositoryService.class);
+    private final Repository defaultRepo = repositoryService.findDefaultRepository();
 
     private String result;
-
 
     public String getResult() {
         return result;
     }
 
-    private static String serializeOrderKey(MoOrder order){
-        return String.format("status=\"%s\",user_group=\"%s\",user=\"%s\"}",order.getStatus(),order.getGroup(),order.getAssignedTo());
+    private static String serializeOrderKey(MoOrder order) {
+        return String.format("status=\"%s\",user_group=\"%s\",user=\"%s\"}",
+            order.getStatus(), order.getGroup(), order.getAssignedTo());
     }
 
-	@Override
-	public void execute(Map<String, Object> parameters) throws BusinessException {
+    @Override
+    public void execute(Map<String, Object> parameters) throws BusinessException {
         result = "# HELP workflow_status_total The total number of workflow instances.\n"
-        +"# TYPE workflow_status_total counter\n";
+            + "# TYPE workflow_status_total counter\n";
         //FIXME: do not agregate in memory, use a SQL query dummy, that is the entire point of storing the entities in SQL
         List<MoOrder> orders = new ArrayList<>();
-        try{
+        try {
             orders = crossStorageApi.find(defaultRepo, MoOrder.class).getResults();
-        } catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         long time = System.currentTimeMillis();
-        String lines ="";
-        Map<String,Map<String,Map<String,Long>>> aggregates = new HashMap<>();
+        String lines = "";
+        Map<String, Map<String, Map<String, Long>>> aggregates = new HashMap<>();
         orders.stream()
-            .collect(Collectors.groupingBy(order -> serializeOrderKey(order), Collectors.counting()))
-            .forEach((id,count)->{result+="workflow_status_total{process=\"order\","+id+" "+count+" "+time+"\n";});
+              .collect(Collectors.groupingBy(WorkflowMetric::serializeOrderKey, Collectors.counting()))
+              .forEach((id, count) -> {
+                  result += "workflow_status_total{process=\"order\"," + id + " " + count + " " + time + "\n";
+              });
     }
 }
