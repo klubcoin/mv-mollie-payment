c1f622ac8cb7dbfa731f52a0e72f7a77700bb2d3
tonysviews <tonysviews@gmail.com>
2022-06-14 06:58:27 +0000

    Add the cet java source file : MoOrder.javain the module : mv-mollie-payment

--- facets/java/org/meveo/model/customEntities/MoOrder.java
+++ facets/java/org/meveo/model/customEntities/MoOrder.java
@@ -24,6 +24,8 @@ public class MoOrder implements CustomEntity {
 
     private String metadata;
 
+    private String assignedTo;
+
     @Override()
     public String getUuid() {
         return uuid;
@@ -55,6 +57,14 @@ public class MoOrder implements CustomEntity {
 
     public void setMetadata(String metadata) {
         this.metadata = metadata;
+    }
+
+    public String getAssignedTo() {
+        return assignedTo;
+    }
+
+    public void setAssignedTo(String assignedTo) {
+        this.assignedTo = assignedTo;
     }
 
     @Override()
