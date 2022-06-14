ebbc009eaaa811dddd4c57def5312039be395dd5
tonysviews <tonysviews@gmail.com>
2022-06-14 06:01:19 +0000

    Add the cet java source file : MoAddress.javain the module : mv-mollie-payment

--- facets/java/org/meveo/model/customEntities/MoAddress.java
+++ facets/java/org/meveo/model/customEntities/MoAddress.java
@@ -19,18 +19,6 @@ public class MoAddress implements CustomEntity {
     @JsonIgnore()
     private DBStorageType storages;
 
-    private String country;
-
-    private String streetAndNumber;
-
-    private String city;
-
-    private String postalCode;
-
-    private String streetAdditional;
-
-    private String region;
-
     @Override()
     public String getUuid() {
         return uuid;
@@ -46,54 +34,6 @@ public class MoAddress implements CustomEntity {
 
     public void setStorages(DBStorageType storages) {
         this.storages = storages;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public String getStreetAndNumber() {
-        return streetAndNumber;
-    }
-
-    public void setStreetAndNumber(String streetAndNumber) {
-        this.streetAndNumber = streetAndNumber;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getPostalCode() {
-        return postalCode;
-    }
-
-    public void setPostalCode(String postalCode) {
-        this.postalCode = postalCode;
-    }
-
-    public String getStreetAdditional() {
-        return streetAdditional;
-    }
-
-    public void setStreetAdditional(String streetAdditional) {
-        this.streetAdditional = streetAdditional;
-    }
-
-    public String getRegion() {
-        return region;
-    }
-
-    public void setRegion(String region) {
-        this.region = region;
     }
 
     @Override()
