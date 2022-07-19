dadb0ea10c7c81acd55d2a358cfc539fe68d2ad0
tonysviews <tonysviews@gmail.com>
2022-07-19 05:43:03 +0000

    Add JS script for Endpoint: moPayOrder

--- facets/javascript/endpoints/moPayOrder.js
+++ facets/javascript/endpoints/moPayOrder.js
@@ -7,7 +7,7 @@ const moPayOrder = async (parameters) =>  {
  			'Content-Type': 'application/json'
 		}),
 		body: JSON.stringify({
-			
+			data : parameters.data
 		})
 	});
 }
@@ -18,16 +18,22 @@ const moPayOrderForm = (container) => {
 			<label for='orderId'>orderId</label>
 			<input type='text' id='moPayOrder-orderId-param' name='orderId'/>
 		</div>
+		<div id='moPayOrder-data-form-field'>
+			<label for='data'>data</label>
+			<input type='text' id='moPayOrder-data-param' name='data'/>
+		</div>
 		<button type='button'>Test</button>
 	</form>`;
 
 	container.insertAdjacentHTML('beforeend', html)
 
 	const orderId = container.querySelector('#moPayOrder-orderId-param');
+	const data = container.querySelector('#moPayOrder-data-param');
 
 	container.querySelector('#moPayOrder-form button').onclick = () => {
 		const params = {
-			orderId : orderId.value !== "" ? orderId.value : undefined
+			orderId : orderId.value !== "" ? orderId.value : undefined,
+			data : data.value !== "" ? data.value : undefined
 		};
 
 		moPayOrder(params).then(r => r.text().then(
