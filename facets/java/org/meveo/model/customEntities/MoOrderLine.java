55f0e67ae0445bafc6b52c0221f5cb934cfc92bd
tonysviews <tonysviews@gmail.com>
2022-09-07 01:26:28 +0000

    Add the cet java source file : MoOrderLine.javain the module : mv-mollie-payment

--- facets/java/org/meveo/model/customEntities/MoOrderLine.java
+++ facets/java/org/meveo/model/customEntities/MoOrderLine.java
@@ -30,6 +30,8 @@ public class MoOrderLine implements CustomEntity {
 
     private String name;
 
+    private Double discountAmount;
+
     private String currency;
 
     private String category;
@@ -95,6 +97,14 @@ public class MoOrderLine implements CustomEntity {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public Double getDiscountAmount() {
+        return discountAmount;
+    }
+
+    public void setDiscountAmount(Double discountAmount) {
+        this.discountAmount = discountAmount;
     }
 
     public String getCurrency() {
