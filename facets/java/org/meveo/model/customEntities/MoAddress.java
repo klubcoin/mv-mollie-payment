2b15f757e94b7ebcc30227a732596fd59ddbf1f5
tonysviews <tonysviews@gmail.com>
2022-09-07 01:26:32 +0000

    Add the cet java source file : MoAddress.javain the module : mv-mollie-payment

--- facets/java/org/meveo/model/customEntities/MoAddress.java
+++ facets/java/org/meveo/model/customEntities/MoAddress.java
@@ -19,6 +19,8 @@ public class MoAddress implements CustomEntity {
     @JsonIgnore()
     private DBStorageType storages;
 
+    private String streetAdditional;
+
     @Override()
     public String getUuid() {
         return uuid;
@@ -34,6 +36,14 @@ public class MoAddress implements CustomEntity {
 
     public void setStorages(DBStorageType storages) {
         this.storages = storages;
+    }
+
+    public String getStreetAdditional() {
+        return streetAdditional;
+    }
+
+    public void setStreetAdditional(String streetAdditional) {
+        this.streetAdditional = streetAdditional;
     }
 
     @Override()
