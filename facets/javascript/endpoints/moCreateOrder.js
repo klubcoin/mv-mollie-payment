50209dbf4f6f55893119e2fe0737dc7362409003
tonysviews <tonysviews@gmail.com>
2022-09-07 01:26:38 +0000

    Create JS script for endpointmoCreateOrder

--- facets/javascript/endpoints/moCreateOrder.js
+++ facets/javascript/endpoints/moCreateOrder.js
@@ -1,34 +1,53 @@
-const moCreateOrder = async (parameters) =>  {
-	const baseUrl = window.location.origin;
-	const url = new URL(`${window.location.pathname.split('/')[1]}/rest/moCreateOrder/`, baseUrl);
-	return fetch(url.toString(), {
-		method: 'POST', 
-		headers : new Headers({
- 			'Content-Type': 'application/json'
-		}),
-		body: JSON.stringify({
-			
-		})
-	});
+import EndpointInterface from "#{API_BASE_URL}/api/rest/endpoint/EndpointInterface.js";
+
+// the request schema, this should be updated
+// whenever changes to the endpoint parameters are made
+// this is important because this is used to validate and parse the request parameters
+const requestSchema = {
+  "title" : "moCreateOrderRequest",
+  "id" : "moCreateOrderRequest",
+  "default" : "Schema definition for moCreateOrder",
+  "$schema" : "http://json-schema.org/draft-07/schema",
+  "type" : "object"
 }
 
-const moCreateOrderForm = (container) => {
-	const html = `<form id='moCreateOrder-form'>
-		<button type='button'>Test</button>
-	</form>`;
-
-	container.insertAdjacentHTML('beforeend', html)
-
-
-	container.querySelector('#moCreateOrder-form button').onclick = () => {
-		const params = {
-
-		};
-
-		moCreateOrder(params).then(r => r.text().then(
-				t => alert(t)
-			));
-	};
+// the response schema, this should be updated
+// whenever changes to the endpoint parameters are made
+// this is important because this could be used to parse the result
+const responseSchema = {
+  "title" : "moCreateOrderResponse",
+  "id" : "moCreateOrderResponse",
+  "default" : "Schema definition for moCreateOrder",
+  "$schema" : "http://json-schema.org/draft-07/schema",
+  "type" : "object",
+  "properties" : {
+    "result" : {
+      "title" : "result",
+      "type" : "string",
+      "minLength" : 1
+    }
+  }
 }
 
-export { moCreateOrder, moCreateOrderForm };
+// should contain offline mock data, make sure it adheres to the response schema
+const mockResult = {};
+
+class moCreateOrder extends EndpointInterface {
+	constructor() {
+		// name and http method, these are inserted when code is generated
+		super("moCreateOrder", "POST");
+		this.requestSchema = requestSchema;
+		this.responseSchema = responseSchema;
+		this.mockResult = mockResult;
+	}
+
+	getRequestSchema() {
+		return this.requestSchema;
+	}
+
+	getResponseSchema() {
+		return this.responseSchema;
+	}
+}
+
+export default new moCreateOrder();
