0dfd51af0f514e0d9636b45edae0834131b8fab6
tonysviews <tonysviews@gmail.com>
2022-09-07 01:26:28 +0000

    Add the cet java source file : MoOrderLine.javain the module : mv-mollie-payment

--- facets/java/org/meveo/model/customEntities/MoOrderLine.java
+++ facets/java/org/meveo/model/customEntities/MoOrderLine.java
@@ -24,25 +24,27 @@ public class MoOrderLine implements CustomEntity {
 
     private Long quantity;
 
-    private String imageUrl;
-
     private Double vatRate;
 
-    private String name;
-
     private Double discountAmount;
+
+    private Instant creationDate;
+
+    private String type;
+
+    private Double vatAmount;
+
+    private Double totalAmount;
+
+    private String imageUrl;
+
+    private String name;
 
     private String currency;
 
     private String category;
 
-    private Instant creationDate;
-
     private String sku;
-
-    private String type;
-
-    private Double vatAmount;
 
     @Override()
     public String getUuid() {
@@ -77,14 +79,6 @@ public class MoOrderLine implements CustomEntity {
         this.quantity = quantity;
     }
 
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
-
     public Double getVatRate() {
         return vatRate;
     }
@@ -93,20 +87,60 @@ public class MoOrderLine implements CustomEntity {
         this.vatRate = vatRate;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public Double getDiscountAmount() {
         return discountAmount;
     }
 
     public void setDiscountAmount(Double discountAmount) {
         this.discountAmount = discountAmount;
+    }
+
+    public Instant getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Instant creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Double getVatAmount() {
+        return vatAmount;
+    }
+
+    public void setVatAmount(Double vatAmount) {
+        this.vatAmount = vatAmount;
+    }
+
+    public Double getTotalAmount() {
+        return totalAmount;
+    }
+
+    public void setTotalAmount(Double totalAmount) {
+        this.totalAmount = totalAmount;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getCurrency() {
@@ -125,36 +159,12 @@ public class MoOrderLine implements CustomEntity {
         this.category = category;
     }
 
-    public Instant getCreationDate() {
-        return creationDate;
-    }
-
-    public void setCreationDate(Instant creationDate) {
-        this.creationDate = creationDate;
-    }
-
     public String getSku() {
         return sku;
     }
 
     public void setSku(String sku) {
         this.sku = sku;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public Double getVatAmount() {
-        return vatAmount;
-    }
-
-    public void setVatAmount(Double vatAmount) {
-        this.vatAmount = vatAmount;
     }
 
     @Override()
