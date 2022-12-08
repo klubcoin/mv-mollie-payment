f1a7cd7437bc449e527e60c0f610986618988bc9
tonysviews <tonysviews@gmail.com>
2022-12-08 11:29:31 +0000

    Add the script File : io.liquichain.api.payment.webhook.OnMollieUpdateOrderin the module : mv-mollie-payment

---
+++ facets/java/io/liquichain/api/payment/webhook/OnMollieUpdateOrder.java
@@ -0,0 +1,15 @@
+package io.liquichain.api.payment.webhook;
+
+import java.util.Map;
+
+import org.meveo.service.script.Script;
+import org.meveo.admin.exception.BusinessException;
+
+public class OnMollieUpdateOrder extends Script {
+	
+	@Override
+	public void execute(Map<String, Object> parameters) throws BusinessException {
+		super.execute(parameters);
+	}
+	
+}
