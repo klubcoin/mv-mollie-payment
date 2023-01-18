a2dbec9157a0fa6945672f9f95d9ae74506e4274
tonysviews <tonysviews@gmail.com>
2023-01-18 08:05:59 +0000

    Create JS script for endpointvalidateMolliePayment

--- facets/javascript/endpoints/validateMolliePayment.js
+++ facets/javascript/endpoints/validateMolliePayment.js
@@ -1,33 +1,53 @@
-const validateMolliePayment = async (parameters) =>  {
-	const baseUrl = window.location.origin;
-	const url = new URL(`${window.location.pathname.split('/')[1]}/rest/validateMolliePayment/${parameters.orderId}`, baseUrl);
-	return fetch(url.toString(), {
-		method: 'GET'
-	});
+import EndpointInterface from "#{API_BASE_URL}/api/rest/endpoint/EndpointInterface.js";
+
+// the request schema, this should be updated
+// whenever changes to the endpoint parameters are made
+// this is important because this is used to validate and parse the request parameters
+const requestSchema = {
+  "title" : "validateMolliePaymentRequest",
+  "id" : "validateMolliePaymentRequest",
+  "default" : "Schema definition for validateMolliePayment",
+  "$schema" : "http://json-schema.org/draft-07/schema",
+  "type" : "object"
 }
 
-const validateMolliePaymentForm = (container) => {
-	const html = `<form id='validateMolliePayment-form'>
-		<div id='validateMolliePayment-orderId-form-field'>
-			<label for='orderId'>orderId</label>
-			<input type='text' id='validateMolliePayment-orderId-param' name='orderId'/>
-		</div>
-		<button type='button'>Test</button>
-	</form>`;
-
-	container.insertAdjacentHTML('beforeend', html)
-
-	const orderId = container.querySelector('#validateMolliePayment-orderId-param');
-
-	container.querySelector('#validateMolliePayment-form button').onclick = () => {
-		const params = {
-			orderId : orderId.value !== "" ? orderId.value : undefined
-		};
-
-		validateMolliePayment(params).then(r => r.text().then(
-				t => alert(t)
-			));
-	};
+// the response schema, this should be updated
+// whenever changes to the endpoint parameters are made
+// this is important because this could be used to parse the result
+const responseSchema = {
+  "title" : "validateMolliePaymentResponse",
+  "id" : "validateMolliePaymentResponse",
+  "default" : "Schema definition for validateMolliePayment",
+  "$schema" : "http://json-schema.org/draft-07/schema",
+  "type" : "object",
+  "properties" : {
+    "result" : {
+      "title" : "result",
+      "type" : "string",
+      "minLength" : 1
+    }
+  }
 }
 
-export { validateMolliePayment, validateMolliePaymentForm };
+// should contain offline mock data, make sure it adheres to the response schema
+const mockResult = {};
+
+class validateMolliePayment extends EndpointInterface {
+	constructor() {
+		// name and http method, these are inserted when code is generated
+		super("validateMolliePayment", "GET");
+		this.requestSchema = requestSchema;
+		this.responseSchema = responseSchema;
+		this.mockResult = mockResult;
+	}
+
+	getRequestSchema() {
+		return this.requestSchema;
+	}
+
+	getResponseSchema() {
+		return this.responseSchema;
+	}
+}
+
+export default new validateMolliePayment();
